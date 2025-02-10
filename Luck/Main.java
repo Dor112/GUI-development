6ed74e8fe6c0f494cb9b6c4d1fa8dd7633b19e48
Artem <125490773+Dor112@users.noreply.github.com>
2025-02-10 09:08:47 +0300

    Update Main.java

--- Luck/Main.java
+++ Luck/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Luck test = new Luck(0,0,0,0);
+        Luck test = new Luck();
         test.lucky();
     }
 }
