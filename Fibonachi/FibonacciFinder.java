31b529837b26f937fb1ed2798fff7485e79146b5
Artem <125490773+Dor112@users.noreply.github.com>
2025-02-16 13:10:12 +0300

    Add files via upload

---
+++ Fibonachi/FibonacciFinder.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class FibonacciFinder {
+    public static boolean isFibonacci(int number) {
+        return isPerfectSquare(5 * number * number + 4) || isPerfectSquare(5 * number * number - 4);
+    }
+
+    private static boolean isPerfectSquare(int x) {
+        int s = (int) Math.sqrt(x);
+        return (s * s == x);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число для проверки: ");
+        int number = scanner.nextInt();
+
+        if (isFibonacci(number)) {
+            System.out.println(number + " является числом Фибоначчи.");
+        } else {
+            System.out.println(number + " не является числом Фибоначчи.");
+        }
+
+        scanner.close();
+    }
+}
