84b2bbe9d9fbb3f396190da74fb8b9f80c4f877e
Artem <125490773+Dor112@users.noreply.github.com>
2025-02-22 16:47:08 +0300

    Update FibonacciFinder.java

--- Fibonachi/FibonacciFinder.java
+++ Fibonachi/FibonacciFinder.java
@@ -1,22 +1,24 @@
 package org.example;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class FibonacciFinder {
-    public static boolean isFibonacci(int number) {
-        return isPerfectSquare(5 * number * number + 4) || isPerfectSquare(5 * number * number - 4);
+    public static boolean isFibonacci(BigInteger number) {
+        return isPerfectSquare(number.multiply(number).multiply(BigInteger.valueOf(10)).add(BigInteger.valueOf(10))) ||
+                isPerfectSquare(number.multiply(number).multiply(BigInteger.valueOf(10)).subtract(BigInteger.valueOf(10)));
     }
 
-    private static boolean isPerfectSquare(int x) {
-        int s = (int) Math.sqrt(x);
-        return (s * s == x);
+    private static boolean isPerfectSquare(BigInteger x) {
+        BigInteger s = BigInteger.valueOf((long) Math.sqrt(x.doubleValue()));
+        return s.multiply(s).equals(x);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите число для проверки(от 1 до 2*10^9): ");
-        int number = scanner.nextInt();
+        BigInteger number = scanner.nextBigInteger();
 
         if (isFibonacci(number)) {
             System.out.println(number + " является числом Фибоначчи.");
